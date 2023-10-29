575baa61bc3254c4b0b177f2036da6e9fd3b6150
elsa-farinella <145492175+elsa-farinella@users.noreply.github.com>
2023-10-29 23:24:15 +0100

    Update run.py

--- run.py
+++ run.py
@@ -21,17 +21,17 @@ def main():
     # For an easier implementation of our functions, we changed the label -1 to 0
     y[y== -1] = 0
 
-    # Then it's time to split the data into training and a local validation set
+    # Then we split the data into training and a local validation set
     train_data, val_data = split_data(data, y, 0.95, seed=13)
 
-    # We split the data into features and labels, since they were put together inside the previous function in ordrer not to lose the correspondence between them
+    # We split the data into features and labels, since they were put together inside the previous function in order not to lose the correspondence between them
     val_x = val_data[:, 0:-1]
     val_y = val_data[:, -1]
 
-    # Before doing the same ting for train data, we drop some rows on it, so that we can train our model on a balanced dataset
+    # Before processing the training data similarly, we remove some rows to ensure a balanced dataset for model training.
     keep_percentage = 0.36
     np.random.seed(13)
-    # we compute the number of zero rows to keep, adn we save the non-zero ones
+    # We compute the number of zero rows to keep, adn we save the non-zero ones
     zero_rows = train_data[train_data[:, -1] == -0]
     non_zero_rows = train_data[train_data[:, -1] != -0]
     num_rows_to_keep = int(len(zero_rows) * keep_percentage)
@@ -41,21 +41,22 @@ def main():
     filtered_data = zero_rows[selected_rows]
     filtered_data = np.concatenate((filtered_data, non_zero_rows), axis=0)
 
-    # Now we can splil features and labels also in train set
+    # Now we can split features and labels also in train set
     train_x = filtered_data[:, :-1]
     train_y = filtered_data[:, -1]
 
+    # We normalize the sets of data
     train_x_normalized = normalize_data(train_x)
     val_x_normalized = normalize_data(val_x)
 
-    # we use a logistic regression model on the normalized data
+    # We use a logistic regression model on the normalized data
     train_tx = build_model_data(train_x_normalized)
     val_tx = build_model_data(val_x_normalized)
     max_iters = 1000
     gamma = 1
     initial_w = np.full(train_tx.shape[1], -1)
     w, loss = logistic_regression(train_y, train_tx, initial_w, max_iters, gamma, False)
-    # we compute some metrics on the validation set to evaluate our model
+    # We compute some metrics on the validation set to evaluate our model
     f1 = compute_f1_logistic(val_y, val_tx, w)
     print("F1 score: ", f1)
     accuracy = compute_accuracy(val_y, val_tx, w)
